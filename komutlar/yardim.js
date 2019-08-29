f3a18efc5b420e9de391eb46cb96c4c3c5af263c
Glitch (fan-marjoram) <none>
2019-08-29 16:43:09 +0000

    🎢🌶 Checkpoint ./komutlar/deneme.js:2288004/1009 ./events/ready.js:2288004/740 ./komutlar/yardim.js:2288004/2626

---
+++ komutlar/yardim.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message) {
+const embed = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle('» Muzik Komut Grupları')
+.setTimestamp()
+.addField('» -çal- Muzik Dinlersiniz', '/çal ')
+.addField('» -Ses-Muzigin Sesin Ayarlarsiniz', '/ses 1/100')
+.addField('» -Geç- Sarkiyi Gecersiniz', '/geç')
+.addField('» -çalan- Calan Sarki Hakkinda Bilgi Verir', '/çalan')
+.addField('» -Duraklat- Sarkiyi Durdurursunuz', '/duraklat')
+.addField('» -Devam- Sarkiyi Devam Ettirirsiniz', '/devam')
+.addField('» -Sira- Kuyrugu Gorursunuz', '/sıra')
+.setFooter('© 2019 Amil Amir Muzik', client.user.avatarURL)
+.setTimestamp()
+.setThumbnail(client.user.avatarURL)
+message.channel.send(embed)
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false, 
+  aliases: ['y','yardım','müzik','muzik','komutlar'], 
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'yardim',
+  description: 'Tüm komutları gösterir.',
+  usage: 'yardim'
+};
